647f15d85e59e82aaca953019bd5b14a0e366df4
irmalien <mikus.vanags@gmail.com>
2019-01-11 19:17:46 +0100

    first init

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// let img;
+
+function preload(){
+  // img - loadImage('kitten.jpg')
+}
+
+function setup() {
+  createCanvas(400, 400,)
+  background(200) 
+}
+
+function draw() {
+  // image(img, 0, 0, mouseX, mouseY)
+  // ellipse(0, 0, 100, 100)
+}

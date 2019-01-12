678f7ad87fde2fb9461e2da9cda99f9bf3f967b0
irmalien <mikus.vanags@gmail.com>
2019-01-12 02:19:03 +0100

    tron theme

--- js/main.js
+++ js/main.js
@@ -1,47 +1,70 @@
 // let img;
+const windowWidth = document.documentElement.clientWidth;
+const windowHeight = document.documentElement.clientHeight;
 
 function preload(){
   // img - loadImage('kitten.jpg')
 }
 let d =8
 let n =5
+let c =0.1;
 let k = 5/8;
 let sliderD;
 let sliderN;
+let sliderC;
+let sliderSize;
 
 function setup() {
-  createCanvas(600, 600,);
-
+  createCanvas(windowWidth, windowHeight*0.9);
+  // angleMode(DEGREES)
   w1=width
   h1=height
-  sliderD=createSlider(1, 30, 4, 1)
-  sliderN=createSlider(1, 30, 4, 1)
+  sliderD=createSlider(1, 30, 14, 1)
+  sliderN=createSlider(1, 30, 15, 1)
+  sliderC=createSlider(1, 100, 4, 0.01)
+  sliderSize=createSlider(10, 1000, 500, 1)
+  frameRate(30)
   // z = TWO_PI-0.1
 
 }
 
 function draw() {
-  background(0);
+  background(0)
   d=sliderD.value();
   n=sliderN.value();
+  step=0.001;
+  c=c+step;
+  // c=sliderC.value();
+
+  size=sliderSize.value();
+  console.log(d,n,c)
   k= n/d;
   // w1++
   // h1++
   // z = z-0.1
   // r=r+0.1;
-  
+  let newC = c
+  let alfa = 255;
   translate(w1/2, h1/2);
   noFill()
-  beginShape();
-  for (let a=0; a<TWO_PI*d; a+=0.001){
-    const r= 200*cos(k*a)
-    const x = r*cos(a);
-    const y = r*sin(a);
-    stroke(255);
-    strokeWeight(1);
-    vertex(x,y);
+  for(let i=1; i<100; i++){
+    let newStep = i*(step/100);
+    newC = newC+newStep;
+    alfa = alfa-4
+    beginShape();
+    for (let a=0; a<TWO_PI*d; a+=TWO_PI/newC){
+      const r= size*cos(k*a)
+      const x = r*cos(a);
+      const y = r*sin(a);
+      stroke(40,240,250, alfa);
+      strokeWeight(0.5);
+      vertex(x,y);
+    }
+    endShape();
   }
-  endShape();
+
+
+  
   // image(img, 0, 0, mouseX, mouseY)
   // ellipse(0, 0, 100, 100)
 }
